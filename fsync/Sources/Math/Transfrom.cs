2571f6ca616133c507e715be9edcd88e3169f758
windyuuy <kugoo11v12@126.com>
2022-04-19 18:01:34 +0800

    U: 更新基础库

--- fsync/Sources/Math/Transfrom.cs
+++ fsync/Sources/Math/Transfrom.cs
@@ -3,11 +3,13 @@ namespace fsync.amath
 {
 	public interface Transform
 	{
-		Transform Parent { get; set; }
 		Vector3 LocalPosition { get; set; }
 		Vector3 LocalScale { get; set; }
 		Quaternion LocalRotation { get; set; }
 
+		Vector3 Position { get; set; }
+		Vector3 Scale { get; }
+		Quaternion Rotation { get; set; }
 	}
 }
 
